813b00fe68546dd7ee31b325fbb243da10a5d355
Víctor Reyes <unhill@gmail.com>
2024-09-10 00:27:01 +0200

    Types

--- precourse/typescript-trainingGround/src/index.test.ts
+++ precourse/typescript-trainingGround/src/index.test.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import assert from 'assert';
-import { greet } from './index';
+import { greet, isOld, countOdd, sumEven, Person, getPersonStreetNo } from './index';
 
 describe('ts tests', () => {
   it('get greeting', () => {
@@ -13,5 +13,81 @@ describe('ts tests', () => {
 
     // assert
     assert.strictEqual(result, 'Hello, Victor, you are 35 years old');
+  });
+
+  it('returns true if age is above or equal to 35', () => {
+    // act
+    const is34Old = isOld(34);
+    const is35Old = isOld(35);
+    const is36Old = isOld(36);
+
+    // assert
+    assert.strictEqual(is34Old, false);
+    assert.strictEqual(is35Old, true);
+    assert.strictEqual(is36Old, true);
+  });
+
+  it('fun with variables', () => {
+    // act
+    const name = 'Marcus';
+    const nameImplicit = 'Marcus';
+    const nameImplicit2 = name;
+
+    const cool = true;
+    const birthYear = 1972;
+
+    // arrange
+    assert.strictEqual(typeof cool, 'boolean');
+    assert.strictEqual(typeof birthYear, 'number');
+    assert.strictEqual(typeof name, 'string');
+    assert.strictEqual(typeof nameImplicit, 'string');
+    assert.strictEqual(typeof nameImplicit2, 'string');
+  });
+
+  it('arrays are typed in ts', () => {
+    const names: string[] = ['Marcus', 'Julia', 'Catherine'];
+    const firstFive = [1, 2, 3, 4, 5];
+    const multipleTypesArray = [1, 'a', true];
+  });
+
+  it('count odd numbers', () => {
+    // arrange
+    const firstFive = [1, 2, 3, 4, 5];
+
+    // act
+    const numberOfOdds = countOdd(firstFive);
+
+    // arrange
+    assert.strictEqual(numberOfOdds, 3);
+  });
+
+  it('sum even numbers', () => {
+    // arrange
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // act
+    const sum = sumEven(nums);
+
+    // arrange
+    assert.strictEqual(sum, 30);
+  });
+
+  it('gets the street number for a person', () => {
+    // arrange
+    const p: Person = {
+      name: 'Marcus',
+      birthYear: 1972,
+      address: {
+        street: 'Strålgatan',
+        streetNo: 23,
+        city: 'Stockholm',
+      },
+    };
+
+    // act
+    const streetNo = getPersonStreetNo(p);
+
+    // assert
+    assert.strictEqual(streetNo, 23);
   });
 });
--- precourse/typescript-trainingGround/src/index.ts
+++ precourse/typescript-trainingGround/src/index.ts
@@ -3,4 +3,32 @@ function greet(name: string, birthYear: number): string {
   return `Hello, ${name}, you are ${age} years old`;
 }
 
-export { greet };
+function isOld(age: number) {
+  return age >= 35;
+}
+
+function countOdd(numbers: Array<number>) {
+  return numbers.filter(n => n % 2 !== 0).length;
+}
+
+function sumEven(numbers: Array<number>) {
+  return numbers.filter(n => n % 2 === 0).reduce((prev, curr) => prev + curr, 0);
+}
+
+type Address = {
+  street: string;
+  streetNo: number;
+  city: string;
+};
+
+type Person = {
+  name: string;
+  birthYear: number;
+  address: Address;
+};
+
+function getPersonStreetNo(person: Person) {
+  return person.address.streetNo;
+}
+
+export { greet, isOld, countOdd, sumEven, Person, Address, getPersonStreetNo };
