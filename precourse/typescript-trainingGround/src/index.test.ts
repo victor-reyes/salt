a3a4435c22982abc3c7997ad9ceb54e0ff732ac2
Víctor Reyes <unhill@gmail.com>
2024-09-09 23:46:47 +0200

    Add and use lint

--- precourse/typescript-trainingGround/src/index.test.ts
+++ precourse/typescript-trainingGround/src/index.test.ts
@@ -1,17 +1,17 @@
-import "mocha";
-import assert from "assert";
-import { greet } from "./index";
+import 'mocha';
+import assert from 'assert';
+import { greet } from './index';
 
-describe("ts tests", () => {
-  it("get greeting", () => {
+describe('ts tests', () => {
+  it('get greeting', () => {
     // arrange
     const birthYear = 1989;
-    const name = "Victor";
+    const name = 'Victor';
 
     // act
     const result = greet(name, birthYear);
 
     // assert
-    assert.strictEqual(result, "Hello, Victor, you are 35 years old");
+    assert.strictEqual(result, 'Hello, Victor, you are 35 years old');
   });
 });
