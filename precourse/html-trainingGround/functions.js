7e6be9beff358ea357196af7fc035c2e0c82c683
Víctor Reyes <unhill@gmail.com>
2024-09-12 14:31:18 +0200

    Adding elements using js

--- precourse/html-trainingGround/functions.js
+++ precourse/html-trainingGround/functions.js
@@ -15,3 +15,15 @@ for (let i = 0; i < boldNodes.length; i++) {
 }
 
 document.querySelectorAll('[name="content"]').forEach(node => (node.innerHTML += '.   Lorem content'));
+
+function addListInMain(numberOfItems) {
+  const main = document.querySelector('main');
+  const ul = main.appendChild(document.createElement('ul'));
+  for (let index = 0; index < numberOfItems; index++) {
+    const li = document.createElement('li');
+    li.innerHTML = `list number ${index + 1}`;
+    ul.appendChild(li);
+  }
+}
+
+addListInMain(4);
