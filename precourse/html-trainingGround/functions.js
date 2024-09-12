7231c0182f2d8bd9b17da7974eff377303b8c982
Víctor Reyes <unhill@gmail.com>
2024-09-12 15:13:36 +0200

    Add  event listeners and buttons.

--- precourse/html-trainingGround/functions.js
+++ precourse/html-trainingGround/functions.js
@@ -8,4 +8,24 @@ const addStyle = selector => {
   });
 };
 
-addStyle('[class]');
+const invertColorsOfResults = () => {
+  const mainResultElements = document.querySelectorAll('.mainResult').forEach(element => {
+    if (element.classList.contains('positiveResult')) {
+      element.classList.remove('positiveResult');
+      element.classList.add('negativeResult');
+      return;
+    }
+
+    if (element.classList.contains('negativeResult')) {
+      element.classList.remove('negativeResult');
+      element.classList.add('positiveResult');
+      return;
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  addStyle('[class]');
+  const btn = document.getElementById('btnInversion');
+  btn.onclick = invertColorsOfResults;
+});
