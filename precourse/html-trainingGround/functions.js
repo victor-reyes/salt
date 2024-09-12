0eed8f24b0a3a5ea683e3997656364eca20715d8
Víctor Reyes <unhill@gmail.com>
2024-09-12 14:12:09 +0200

    Try out getElementsBy{...}

--- precourse/html-trainingGround/functions.js
+++ precourse/html-trainingGround/functions.js
@@ -8,3 +8,11 @@ const companyName = myFunctions.getCompanyName();
 document.getElementsByTagName('div')[1].innerHTML = companyName;
 
 document.getElementById('companyName').innerHTML = companyName;
+
+const boldNodes = document.getElementsByClassName('bold');
+
+for (let i = 0; i < boldNodes.length; i++) {
+  boldNodes[i].innerHTML += ' ... and bold';
+}
+
+const contentNodes = document.getElementsByName('content').forEach(node => (node.innerHTML += '.   Lorem content'));
