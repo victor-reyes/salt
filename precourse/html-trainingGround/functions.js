f1b416a33a7dc4d145072164b0d35112b2641cab
Víctor Reyes <unhill@gmail.com>
2024-09-12 15:01:50 +0200

    Style using js

--- precourse/html-trainingGround/functions.js
+++ precourse/html-trainingGround/functions.js
@@ -1,34 +1,11 @@
-const myFunctions = {
-  getCompanyName: function () {
-    return 'SALT';
-  },
+const addStyle = selector => {
+  document.querySelectorAll(selector).forEach(e => {
+    const possibleNumber = Number(e.innerHTML);
+
+    if (Number.isNaN(possibleNumber)) return;
+
+    e.classList.add(possibleNumber < 0 ? 'negativeResult' : 'positiveResult');
+  });
 };
 
-const companyName = myFunctions.getCompanyName();
-
-const companyNameElement = document.querySelector('#companyName');
-companyNameElement.innerHTML = companyName + ' set using a query selector by id';
-
-const boldNodes = document.querySelectorAll('.bold');
-for (let i = 0; i < boldNodes.length; i++) {
-  boldNodes[i].innerHTML += ' ... and bold selected with query';
-}
-
-document.querySelectorAll('[name="content"]').forEach(node => (node.innerHTML += '.   Lorem content'));
-
-function addListInMain(numberOfItems) {
-  const main = document.querySelector('main');
-  const ul = main.appendChild(document.createElement('ul'));
-  for (let index = 0; index < numberOfItems; index++) {
-    const li = document.createElement('li');
-    li.innerHTML = `list number ${index + 1}`;
-    ul.appendChild(li);
-  }
-}
-
-function removeDivBy(id){
-  document.querySelector(`div#${id}`)?.remove()
-}
-
-addListInMain(4);
-removeDivBy('content3')
+addStyle('[class]');
