ab3800e4d83951359bf0429357a604556e537043
Víctor Reyes <unhill@gmail.com>
2024-09-13 14:38:43 +0200

    Add filter mechanism

--- precourse/html-trainingGround/fetch.js
+++ precourse/html-trainingGround/fetch.js
@@ -1,18 +1,29 @@
+let allUsers;
+let users;
+
 const getUsers = async () => {
   const response = await fetch('https://randomuser.me/api/?gender=female&results=30');
   const json = await response.json();
   console.log(json);
+  allUsers = users = json.results;
+};
 
-  return json.results;
+const filterData = async name => {
+  console.log(name);
+  name = name.toLowerCase();
+  users = allUsers.filter(
+    user => user.name.first.toLowerCase().includes(name) || user.name.last.toLowerCase().includes(name)
+  );
 };
 
 const showUsers = async () => {
-  const users = await usersPromise;
-
   const main = document.querySelector('main');
+  main.childNodes.forEach(node => node.remove());
+
   main.textContent = `We have fetched ${users.length} users`;
 
   const userList = document.createElement('div');
+  userList.classList = 'column';
 
   users.forEach(user => {
     const userHeading = document.createElement('h2');
@@ -20,10 +31,13 @@ const showUsers = async () => {
 
     const userImage = document.createElement('img');
     userImage.src = `${user.picture.thumbnail}`;
+    userImage.style.height = '48px';
+    userImage.style.width = 'auto';
 
     const userDiv = document.createElement('div');
-    userDiv.appendChild(userHeading);
+    userDiv.classList = 'row';
     userDiv.appendChild(userImage);
+    userDiv.appendChild(userHeading);
 
     userList.appendChild(userDiv);
   });
@@ -31,8 +45,13 @@ const showUsers = async () => {
   main.appendChild(userList);
 };
 
-const usersPromise = getUsers();
+getUsers().then(() => showUsers());
 
-document.addEventListener('DOMContentLoaded', () => {
-  showUsers();
+const filterForm = document.getElementById('filterForm');
+filterForm.addEventListener('submit', async event => {
+  event.preventDefault(); // Prevent page refresh on form submit
+
+  const filterValue = document.getElementById('filter').value;
+  await filterData(filterValue);
+  await showUsers();
 });
