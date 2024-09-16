511ccfda3b1c9db75d1ae89b8c7f4d47cf27e9af
Víctor Reyes <unhill@gmail.com>
2024-09-16 22:07:14 +0200

    Mock database and implement proper GET API

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -2,9 +2,27 @@ import express from 'express';
 
 const app = express();
 
-const futureDev = { id: 1, name: 'Víctor Reyes', email: 'victor.reyes@salt.dev' };
-app.get('/', (req, res) => {
-  res.status(201).set('location', '/api/developers/1').json(futureDev);
+const db = [
+  {
+    id: 1,
+    name: 'Marcus Dev',
+    email: 'marcus@salt.dev',
+  },
+  {
+    id: 2,
+    name: 'Víctor Reyes',
+    email: 'victor.reyes@salt.dev',
+  },
+];
+
+app.get('/api/developers', (_req, res) => {
+  res.json(db);
+});
+
+app.get('/api/developers/:id', (req, res) => {
+  const dev = db.find(d => d.id === Number(req.params.id));
+
+  return dev ? res.json(dev) : res.status(404).end();
 });
 
 const port = 3000;
