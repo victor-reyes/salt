d7d09c98fb634f41753d9bfe3c846ea1c03d3969
Víctor Reyes <unhill@gmail.com>
2024-09-16 21:35:07 +0200

    Use morgon as logging-middleware

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -1,12 +1,8 @@
 import express from 'express';
-
-const saltLogger = (req, res, next) => {
-  console.log(`salt> ${req.method} - ${req.url}`);
-  next();
-};
+import morgan from 'morgan';
 
 const app = express();
-app.use(saltLogger);
+app.use(morgan('tiny'));
 app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.send('Hello fellow developer!');
