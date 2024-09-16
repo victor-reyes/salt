edf3188d73282a40b896274f29de5f7691252f94
Víctor Reyes <unhill@gmail.com>
2024-09-16 12:46:49 +0200

    Add basic server

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Started...');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello fellow developer!');
+});
+
+const hostname = 'localhost';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
