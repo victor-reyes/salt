ac9b75486902446fba962928d0e3cd12a75f48f9
Víctor Reyes <unhill@gmail.com>
2024-09-16 21:32:12 +0200

    Add own logging-middleware

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
+const saltLogger = (req, res, next) => {
+  console.log(`salt> ${req.method} - ${req.url}`);
+  next();
+};
+
 const app = express();
+app.use(saltLogger);
 app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.send('Hello fellow developer!');
