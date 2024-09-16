e1ef1a661b4da71b23c143937850927d9a9a8185
Víctor Reyes <unhill@gmail.com>
2024-09-16 22:49:45 +0200

    Implement POST method

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -15,6 +15,8 @@ const db = [
   },
 ];
 
+app.use(express.json());
+
 app.get('/api/developers', (_req, res) => {
   res.json(db);
 });
@@ -23,6 +25,14 @@ app.get('/api/developers/:id', (req, res) => {
   const dev = db.find(d => d.id === Number(req.params.id));
 
   return dev ? res.json(dev) : res.status(404).end();
+});
+
+app.post('/api/developers/', (req, res) => {
+  const id = Math.max(0, ...db.map(dev => dev.id)) + 1;
+  const { name, email } = req.body;
+  const newDev = { id, name, email };
+  db.push(newDev);
+  res.status(201).set('location', `/api/developers/${id}`).json(newDev);
 });
 
 const port = 3000;
