5fd12136a4f88c46ba766ab3cc1f9b91f501505a
Víctor Reyes <unhill@gmail.com>
2024-09-16 21:16:28 +0200

    Handle different path and introduce 404 responce

--- precourse/webapiJSTrainingGround/src/index.ts
+++ precourse/webapiJSTrainingGround/src/index.ts
@@ -1,8 +1,27 @@
 import http from 'http';
 import fs from 'fs';
 
-const server = http.createServer((_req, res) => {
-  const content = fs.readFileSync('./static/index.html', 'utf-8');
+const getPathFrom = (url: string | undefined) => {
+  if (!url || url.substring(1) === '') return 'index.html';
+  return url.substring(1);
+};
+
+const getFileContentOr404 = (path: string) => {
+  let name = path;
+  if (!fs.existsSync(`./static/${name}`)) name = '404.html';
+
+  return fs.readFileSync(`./static/${name}`, 'utf-8');
+};
+
+const server = http.createServer((req, res) => {
+  const path = getPathFrom(req.url);
+  if (path === 'favicon.ico') {
+    res.statusCode = 404;
+    res.end('');
+    return;
+  }
+
+  const content = getFileContentOr404(path);
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
