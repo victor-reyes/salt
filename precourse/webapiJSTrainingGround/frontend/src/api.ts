281c18edc549690df9a8d7fbabe25f03fb416bd6
Víctor Reyes <unhill@gmail.com>
2024-09-17 16:37:44 +0200

    Add the rest of methods

--- precourse/webapiJSTrainingGround/frontend/src/api.ts
+++ precourse/webapiJSTrainingGround/frontend/src/api.ts
@@ -1,18 +1,54 @@
 const BASE_URL = 'http://localhost:3000/api/developers';
 
+type Dev = {
+  id: number;
+  name: string;
+  email: string;
+};
 const getAllDevs = async () => {
   const response = await fetch(BASE_URL);
-  const json = await response.json();
-  return json;
+  const devs: Dev[] = await response.json();
+  return devs;
 };
 
-const getDevById = async (id: number) => {
-  const response = await fetch(`${BASE_URL}/${id}`);
-  const json = await response.json();
-  return json;
+const deleteDev = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/${id}`, { method: 'DELETE' });
+
+  if (!response.ok)
+    throw new Error(`Failed to delete developer with error ${response.status} because of ${response.statusText}`);
+
+  return response.status;
+};
+
+const addDev = async (name: string, email: string) => {
+  const response = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email }),
+  });
+
+  if (!response.ok)
+    throw new Error(`Failed to add developer with error ${response.status} because of ${response.statusText}`);
+
+  return (await response.json()) as Dev;
+};
+
+const updateDev = async (dev: Dev) => {
+  const response = await fetch(`${BASE_URL}/${dev.id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: dev.name, email: dev.email }),
+  });
+
+  if (!response.ok)
+    throw new Error(`Failed to update developer with error ${response.status} because of ${response.statusText}`);
+
+  return (await response.json()) as Dev;
 };
 
 export const api = {
   getAllDevs,
-  getDevById,
+  deleteDev,
+  addDev,
+  updateDev,
 };
